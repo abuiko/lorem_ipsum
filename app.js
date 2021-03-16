d20485af6e617276d78efba6fd79c6a56da56070
Alena Buiko <abuiko.ma@gmail.com>
2021-03-15 20:27:29 -0400

    create array with 9 lorem paragraphs

---
+++ app.js
@@ -0,0 +1,56 @@
+// lorem ipsum
+
+const text = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+    labore et dolore magna aliqua. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. 
+    Purus ut faucibus pulvinar elementum integer enim. Hendrerit gravida rutrum quisque non tellus orci ac auctor. 
+    Id diam vel quam elementum pulvinar. Egestas egestas fringilla phasellus faucibus scelerisque. 
+    In dictum non consectetur a erat nam at lectus urna. Ante metus dictum at tempor`,
+
+    `Feugiat in ante metus dictum at tempor commodo ullamcorper a. Eleifend quam adipiscing vitae proin sagittis 
+    nisl rhoncus mattis. Sed felis eget velit aliquet sagittis id. Dictumst vestibulum rhoncus est pellentesque.
+     Et ultrices neque ornare aenean euismod. Magna fermentum iaculis eu non diam phasellus vestibulum lorem sed. 
+     Fermentum iaculis eu non diam phasellus vestibulum lorem. Cras fermentum odio eu feugiat pretium nibh 
+     ipsum consequat nisl. Tristique nulla aliquet enim tortor. Tincidunt augue interdum velit euismod in`,
+
+    `Suscipit adipiscing bibendum est ultricies integer quis. Diam ut venenatis tellus in metus vulputate eu. 
+    Lorem ipsum dolor sit amet consectetur. Diam ut venenatis tellus in metus vulputate eu scelerisque. Consectetur 
+    libero id faucibus nisl tincidunt eget nullam. Mollis aliquam ut porttitor leo. Egestas erat imperdiet sed 
+    euismod nisi porta lorem mollis aliquam. Duis ut diam quam nulla porttitor massa. Pellentesque adipiscing 
+    commodo elit at imperdiet dui accumsan. Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Sem 
+    et tortor consequat id porta nibh. Sed odio morbi quis commodo odio. Porttitor massa id neque aliquam vestibulum`,
+
+    `Et netus et malesuada fames ac. Etiam tempor orci eu lobortis elementum nibh tellus molestie. Enim 
+    sit amet venenatis urna cursus. Diam vel quam elementum pulvinar etiam. Dui id ornare arcu odio ut
+    sem nulla pharetra diam. Risus quis varius quam quisque id diam vel quam elementum. Egestas dui id o
+    rnare arcu odio ut sem nulla. Egestas maecenas pharetra convallis posuere morbi. Id ornare arcu odio
+    ut sem nulla pharetra diam sit. Pulvinar `,
+
+    `Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Fringilla ut morbi 
+    tincidunt augue interdum. Sit amet est placerat in egestas erat. Rhoncus aenean vel elit scelerisque mauris 
+    pellentesque. Est ultricies integer quis auctor elit sed. Morbi tristique senectus et netus et malesuada 
+    fames. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Diam volutpat commodo sed 
+    egestas egestas fringilla phasellus`,
+
+    `Leo duis ut diam quam nulla porttitor. Aliquam vestibulum morbi blandit cursus. Donec et odio pellentesque
+     diam volutpat. Turpis tincidunt id aliquet risus feugiat in ante metus dictum elit scelerisque mauris 
+     pellentesque. Est ultricies integer quis auctor elit sed. Morbi tristique senectus et netus et malesuada 
+     fames. Enim nulla`,
+
+    `pellentesque habitant morbi tristique senectus et netus et malesuada. Egestas quis ipsum suspendisse ultrices 
+    gravida dictum fusce ut placerat. Vitae elementum curabitur vitae nunc sed velit dignissim. Et tortor at risus
+     viverra adipiscing. Convallis tellus id interdum velit. Interdum posuere lorem ipsum dolor sit amet consectetur 
+     adipiscing. Lectus proin nibh nisl condimentum id venenatis a`,
+
+    `Facilisi etiam dignissim diam quis. Urna molestie at elementum eu facilisis sed. Faucibus nisl tincidunt 
+    eget nullam non. Diam in arcu cursus euismod. Sed elementum tempus egestas sed sed risus. Urna duis convallis
+     convallis tellus. Viverra nam libero justo laoreet sit. Est pellentesque elit ullamcorper dignissim cras
+      tincidunt. Mi proin sed libero enim sed faucibus turpis in eu. Sed nisi lacus sed viverra tellus in hac 
+      habitasse. Diam sollicitudin tempor id eu nisl. Pharetra convallis posuere morbi leo urna molestie`,
+
+    `Mauris vitae ultricies leo integer. Id cursus metus aliquam eleifend. Eu nisl nunc mi ipsum faucibus vitae 
+    aliquet nec. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Venenatis tellus in metus 
+    vulputate eu scelerisque felis imperdiet proin. Sodales neque sodales ut etiam sit amet nisl. Ultricies integer q
+    uis auctor elit sed vulputate mi sit amet. Aliquam vestibulum morbi blandit cursus risus at. Et odio 
+    pellentesque diam volutpat commodo sed egestas egestas fringilla. Phasellus faucibus scelerisque eleifend donec`
+]
